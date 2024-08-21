91d68bf84adb2c2267716b38180dfa7fea09d043
Logic Tanjueco <logic.tanjueco12@gmail.com>
2024-08-22 01:40:21 +0800

    CHORE: Calculate totals in quotations

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -5,6 +5,27 @@ class Quotation < ApplicationRecord
   has_many :products, dependent: :destroy, inverse_of: :quotation
   accepts_nested_attributes_for :products, allow_destroy: true, reject_if: :all_blank
 
-  # enum :company, { GAT: 0, GCMES: 1 }
   enum :payment, [ "50% downpayment", "30 days", "Paid" ]
+
+  before_create :compute_totals
+
+  def compute_totals
+    compute_sub_total
+    compute_value_added_tax
+    compute_total_amount
+  end
+
+  private
+
+  def compute_sub_total
+    self.sub_total = products.sum { |product| product.price * product.quantity }
+  end
+
+  def compute_value_added_tax
+    self.vat = self.sub_total * 0.12
+  end
+  
+  def compute_total_amount
+    self.total = self.sub_total + self.vat
+  end
 end
