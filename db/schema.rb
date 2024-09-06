ad7c5c57f446b37e1458cd9a3decca7e6b2f43ba
Logic Tanjueco <logic.tanjueco12@gmail.com>
2024-09-06 13:31:04 +0800

    FEAT: Employees

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_06_043154) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_06_050105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_06_043154) do
     t.string "code"
     t.text "address"
     t.bigint "tin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "contact_number"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
