023f43ecc0f62bcde3859e7b7fbc8dbde1983ec2
Logic Tanjueco <logic.tanjueco12@gmail.com>
2024-10-14 15:19:34 +0800

    Index pages views

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,7 @@
 class MainController < ApplicationController
+  def index
+    @request_forms = RequestForm.all
+    @purchase_orders = PurchaseOrder.all
+    @quotations = Quotation.all
+  end
 end
