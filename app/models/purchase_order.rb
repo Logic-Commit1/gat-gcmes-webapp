0b8e4a70c51a71180a6b949653100d0039343512
Logic Tanjueco <logic.tanjueco12@gmail.com>
2024-12-01 16:26:06 +0800

    Minor Styling

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -3,10 +3,9 @@ class PurchaseOrder < ApplicationRecord
   belongs_to :supplier
   belongs_to :employee
   # belongs_to :project, optional: true
-
+  has_many :request_forms
   has_many :products, through: :request_forms
   has_many :particulars, through: :request_forms
-  has_many :request_forms
 
   enum :terms, [ "50% downpayment", "30 days", "Paid" ]
 end
