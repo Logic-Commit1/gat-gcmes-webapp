420b3581d4efc75b574bd43f32e58fa1ed2fadb6
Logic Tanjueco <logic.tanjueco12@gmail.com>
2024-12-21 02:40:17 +0800

    Wip: Canvass Create Form | Canvass Pdf View

--- app/javascript/controllers/shared_fields_controller.js
+++ app/javascript/controllers/shared_fields_controller.js
@@ -11,41 +11,44 @@ export default class extends Controller {
   ]
 
   connect() {
-    // this.element.addEventListener("submit", this.sync.bind(this))
+    this.element.addEventListener("submit", this.sync.bind(this))
   }
 
-  // checkFields(event) {
-  //   event.preventDefault()
-  //   this.sync(event)
+  checkFields(event) {
+    event.preventDefault()
+    this.sync(event)
 
-  //   console.log(
-  //     this.descriptionFieldTargets.map((field) => field.value),
-  //     this.quantityFieldTargets.map((field) => field.value),
-  //     this.unitFieldTargets.map((field) => field.value)
-  //   )
-  // }
+    console.log(
+      this.descriptionFieldTargets.map((field) => field.value),
+      this.quantityFieldTargets.map((field) => field.value),
+      this.unitFieldTargets.map((field) => field.value)
+    )
+  }
 
-  // checkValues(event) {
-  //   event.preventDefault()
+  checkValues(event) {
+    event.preventDefault()
 
-  //   const descriptionValue = this.descriptionFieldTarget.value
-  //   const quantityValue = this.quantityFieldTarget.value
-  //   const unitValue = this.unitFieldTarget.value
+    const descriptionValue = this.descriptionFieldTarget.value
+    const quantityValue = this.quantityFieldTarget.value
+    const unitValue = this.unitFieldTarget.value
 
-  //   console.log(descriptionValue, quantityValue, unitValue)
-  // }
+    console.log(descriptionValue, quantityValue, unitValue)
+  }
 
   sync() {
+    console.log("1")
     const descriptionValue = document.querySelector(
       'input[name="canvass[description]"]'
     ).value
+    console.log("2")
     const quantityValue = document.querySelector(
       'input[name="canvass[quantity]"]'
     ).value
+    console.log("3")
     const unitValue = document.querySelector(
-      'input[name="canvass[unit]"]'
+      'select[name="canvass[unit]"]'
     ).value
-
+    console.log("4")
     this.descriptionFieldTargets.forEach(
       (field) => (field.value = descriptionValue)
     )
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  UNIT_OF_MEASUREMENTS = %w[pcs cm m km in ft yd mi].freeze
 end
