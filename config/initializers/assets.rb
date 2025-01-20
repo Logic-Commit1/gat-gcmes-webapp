e3d107f1c7193f71ed203621f654f8420d66553f
Logic Tanjueco <logic.tanjueco12@gmail.com>
2025-01-21 02:07:05 +0800

    Precompile image assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,8 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("app/assets/images")
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -12,3 +14,4 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(styles.scss)
 Rails.application.config.assets.precompile += ['blueprint/screen.css', 'pdf.css', 'jquery.ui.datepicker.js', 'pdf.js']
+Rails.application.config.assets.precompile += %w( *.png )
