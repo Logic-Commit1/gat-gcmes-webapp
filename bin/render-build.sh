559be350ffb34bbd0cefa1e42df90d2511988608
Logic Tanjueco <logic.tanjueco12@gmail.com>
2025-01-23 03:25:47 +0800

    Update render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -32,17 +32,17 @@ npm install
 npm list
 
 # Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
-  echo "...Copying Puppeteer Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR || { echo "Failed to copy Puppeteer cache"; exit 1; }
-else 
-  echo "...Storing Puppeteer Cache in Build Cache" 
-  if [[ "$PUPPETEER_CACHE_DIR" != "$XDG_CACHE_HOME" ]]; then
-    cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME || { echo "Failed to store Puppeteer cache"; exit 1; }
-  else
-    echo "Source and destination are the same. Skipping copy."
-  fi
-fi
+# if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+#   echo "...Copying Puppeteer Cache from Build Cache" 
+#   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR || { echo "Failed to copy Puppeteer cache"; exit 1; }
+# else 
+#   echo "...Storing Puppeteer Cache in Build Cache" 
+#   if [[ "$PUPPETEER_CACHE_DIR" != "$XDG_CACHE_HOME" ]]; then
+#     cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME || { echo "Failed to store Puppeteer cache"; exit 1; }
+#   else
+#     echo "Source and destination are the same. Skipping copy."
+#   fi
+# fi
 
 bundle install
 bundle exec rails assets:precompile
