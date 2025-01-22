9e82e49275190b0d54bbfd78ca34db0ab40590fc
Logic Tanjueco <logic.tanjueco12@gmail.com>
2025-01-23 01:43:00 +0800

    Update render-build file and config file name

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -15,14 +15,14 @@ rm -rf package-lock.json node_modules
 npm install
 npm list
 
-# # Store/pull Puppeteer cache with build cache
-# if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
-#   echo "...Copying Puppeteer Cache from Build Cache" 
-#   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
-# else 
-#   echo "...Storing Puppeteer Cache in Build Cache" 
-#   cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
-# fi
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+  echo "...Copying Puppeteer Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else 
+  echo "...Storing Puppeteer Cache in Build Cache" 
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+fi
 
 bundle install
 bundle exec rails assets:precompile
