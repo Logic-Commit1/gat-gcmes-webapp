b54c612235038e0abf4b754cd60722ef88a2b056
Logic Tanjueco <logic.tanjueco12@gmail.com>
2025-01-23 00:24:10 +0800

    Update render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,16 +2,22 @@
 # exit on error
 set -o errexit
 
+# Install Puppeteer globally
 npm install puppeteer --global
+
+# Install Chromium (used by Puppeteer)
 npx puppeteer browsers install chrome
+
+# Echo to verify if Puppeteer is installed and Chrome is available
 echo "Puppeteer installed and Chrome is being fetched..."
 
-# Check if the Chrome executable is available
-if command -v google-chrome-stable &> /dev/null
-then
-    echo "Chrome is installed successfully."
+# Check if the Chrome executable is available in Puppeteer's installed path
+CHROME_PATH="/opt/render/.cache/puppeteer/chrome/linux-131.0.6778.87/chrome-linux64/chrome"
+if [ -f "$CHROME_PATH" ]; then
+    echo "Chrome installed at $CHROME_PATH"
+    export PUPPETEER_EXECUTABLE_PATH="$CHROME_PATH"
 else
-    echo "Chrome installation failed or not found."
+    echo "Chrome installation failed or not found at $CHROME_PATH"
 fi
 
 bundle install
