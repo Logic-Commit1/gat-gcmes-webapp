9ae6f5fcd68d11de04d67c18e9575b9269b032a7
Logic Tanjueco <logic.tanjueco12@gmail.com>
2025-01-23 01:17:12 +0800

    Changed puppeteer config file name

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,25 +3,25 @@
 set -o errexit
 
 # Install Puppeteer globally
-npm install puppeteer
-npm install puppeteer-core
+# npm install puppeteer
+# npm install puppeteer-core
 
 # Install Chromium (used by Puppeteer)
-npx puppeteer browsers install chrome
+# npx puppeteer browsers install chrome
 
 # Echo to verify if Puppeteer is installed and Chrome is available
-echo "Puppeteer installed and Chrome is being fetched..."
+# echo "Puppeteer installed and Chrome is being fetched..."
 
 npm list
 
-# Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
-  echo "...Copying Puppeteer Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
-else 
-  echo "...Storing Puppeteer Cache in Build Cache" 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
-fi
+# # Store/pull Puppeteer cache with build cache
+# if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+#   echo "...Copying Puppeteer Cache from Build Cache" 
+#   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+# else 
+#   echo "...Storing Puppeteer Cache in Build Cache" 
+#   cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+# fi
 
 bundle install
 bundle exec rails assets:precompile
