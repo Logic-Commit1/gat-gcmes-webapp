410cc42b0993509eb9bc049344f0c31bac6fa49f
Logic Tanjueco <logic.tanjueco12@gmail.com>
2025-02-07 13:59:37 +0800

    Add rails logger

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,7 +14,7 @@ class ProfilesController < ApplicationController
   
       begin
         current_user.signature.attach(params[:user][:signature])
-        Rails.logger.info "Signature attached successfully."
+        Rails.logger.info "Signature attached successfully1."
       rescue => e
         Rails.logger.error "Error attaching signature: #{e.message}"
         redirect_to profile_path, alert: 'Failed to attach signature'
@@ -23,16 +23,21 @@ class ProfilesController < ApplicationController
   
       # Ensure Cloudflare R2 does not enforce checksum
       # current_user.signature.blob.update!(checksum: nil) if Rails.env.production?
+      Rails.logger.info "Signature attached successfully2."
   
       # Generate variant after successful attachment
       if current_user.signature.attached?
+        Rails.logger.info "Signature attached successfully3."
         current_user.signature.variant(resize_to_limit: [300, 100]).processed
       end
+      Rails.logger.info "Signature attached successfully4."
     end
   
     if current_user.save
+      Rails.logger.info "Signature updated successfully1."
       redirect_to profile_path, notice: 'Signature updated successfully'
     else
+      Rails.logger.info "Signature updated not successfully2 due to #{current_user.errors.full_messages}"
       redirect_to profile_path, alert: 'Failed to update signature'
     end
   end
