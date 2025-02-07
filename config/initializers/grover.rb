36566ccb2fc0487c7323d49e01674cd1038e981a
Logic Tanjueco <logic.tanjueco12@gmail.com>
2025-02-07 12:05:51 +0800

    Optimize grover config

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,4 +1,3 @@
-
 Grover.configure do |config|
   config.options = {
     executable_path: Rails.env.production? ? '/opt/render/project/src/chrome/linux-131.0.6778.87/chrome-linux64/chrome' : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
@@ -12,14 +11,27 @@ Grover.configure do |config|
     prefer_css_page_size: true,
     print_background: true,
     emulate_media: 'screen',
-    wait_until: 'networkidle0',
+    wait_until: 'domcontentloaded',
     viewport: {
       width: 794,  # A4 width in pixels at 96 DPI
       height: 1123 # A4 height in pixels at 96 DPI
-    }
+    },
+    cache: true,
+    scale: 1,
+    timeout: 10_000,
+    launch_args: [
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--disable-web-security',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-extensions',
+      '--disable-software-rasterizer',
+      '--headless'
+    ]
   }
-  config.use_png_middleware = true
-  config.use_jpeg_middleware = true
+  config.use_png_middleware = false
+  config.use_jpeg_middleware = false
   config.use_pdf_middleware = false
 
   # Set environment-specific root URL
